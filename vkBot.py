747d064859af3b013a65a262f6c4adf85bb4ee68
mezhendosina <80736171+mezhendosina@users.noreply.github.com>
2021-08-30 20:33:09 +0500

    add Кинг-конг, Пинг-Понг

--- vkBot.py
+++ vkBot.py
@@ -3,6 +3,13 @@ from vkbottle.bot import Bot, Message
 from extractHomeworkFromHTML import selectHomework
 
 bot = Bot(os.getenv('VK_API_TOKEN'))
+
+@bot.on.message(text=['Кинг', 'Конг'])
+async def sendYesterdayHomework(message: Message):
+    await message.answer('Конг')
+@bot.on.message(text=['Пинг', 'ПИНГ'])
+async def sendYesterdayHomework(message: Message):
+    await message.answer('Понг')
 @bot.on.message(text=['Че задали', '/che'])
 async def sendHomework(message: Message):
     print('request homework')
@@ -13,7 +20,9 @@ async def sendYesterdayHomework(message: Message):
 @bot.on.message(text=['/today'])
 async def sendTodayHomework(message: Message):
     await message.answer(str(selectHomework(0)))
-
-#async def sendTimeHomework():
-bot.api.messages.send(peer_id=2000000163, random_id=0, message='.')
+'''
+@bot.on.messag
+async def sendTimeHomework():
+    await bot.api.messages.send(peer_id=2000000163, random_id=0, message='.')
+'''
 bot.run_forever()

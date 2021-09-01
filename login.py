86011d08e53988835da816e6ef1cee1be3db2054
mezhendosina <mensh.zheya@gmail.com>
2021-09-01 15:12:08 +0500

    add chrome_options

--- login.py
+++ login.py
@@ -10,7 +10,9 @@ import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("google-chrome")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_SHIM', None)
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)

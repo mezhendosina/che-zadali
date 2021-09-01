66c04941b4f69cc7c6d5c51addb50753a870e3f4
mezhendosina <mensh.zheya@gmail.com>
2021-09-01 13:17:31 +0500

    debug

--- extractHomeworkFromHTML.py
+++ extractHomeworkFromHTML.py
@@ -1,24 +1,29 @@
 from bs4 import BeautifulSoup
-import psycopg2
-import os 
 from datetime import datetime, timedelta
-import time 
+import psycopg2
+import pytz
+import os 
+
+summerHolidays = ['06', '07', '08']
+holidays = [
+	'27.10.2021', '28.10.2021', '29.03.2021,', '30.10.2021', '31.10.2021', 
+	'01.11.2021', '02.11.2021', '03.11.2021', '29.12.2021', '30.12.2021', '31.12.2021', '01.01.2022', '02.01.2022', '03.01.2022', '04.01.2022', '05.01.2022', '06.01.2022', '07.01.2022', '08.01.2022', '09.01.2022', '10.01.2022', '11.01.2022', '12.01.2022', 
+	'22.03.2022', '23.03.2022', '24.03.2022', '25.03.2022', '26.03.2022', '27.03.2022', '28.03.2022', '29.03.2022'
+	]
+
 def extractHomework(code):
 	def months(month):
 		return {
-			'янв.': 1,
-			'февр.': 2,
-			'мар.': 3,
-			'апр.': 4,
-			'мая': 5,
-			'сент.': 9,
-			'окт.': 10,
-			'нояб.': 11,
-			'дек.': 12,
+			'дек.': 12,'янв.': 1, 'февр.': 2,
+			'мар.': 3,'апр.': 4,'мая': 5,
+			'июн.': 6, 'июл.': 7, 'авг.': 8,
+			'сент.': 9, 'окт.': 10, 'нояб.': 11
 		}[month]
 	connection = psycopg2.connect(os.getenv('DATABASE_URL'), sslmode='require')
 	cursor = connection.cursor()
-
+	'''
+	cursor.execute('SELECT count(*) FROM homeworktable;'))
+	'''
 	soup =  BeautifulSoup(code, features = 'lxml')
 	schoolJournal = soup.find('div', 'schooljournal_content column')
 	dayTable = schoolJournal.find_all('div', class_ = 'day_table')
@@ -35,34 +40,61 @@ def extractHomework(code):
 					homework = a.find('a', 'ng-binding ng-scope').get_text()
 				except AttributeError:
 					homework = None
-				cursor.execute("INSERT INTO homeworktable VALUES('{}', '{}', '{}', {}, {}, {}, {})  ON CONFLICT DO NOTHING".format(
-					day, lesson, homework, dayName, dayNum, dayMonth, dayYear
-				))
+				cursor.execute(f"INSERT INTO homeworktable VALUES('{day}', '{lesson}', '{homework}', '{dayName}', {dayNum}, {dayMonth}, {dayYear})  ON CONFLICT DO NOTHING"
+				)
 			except AttributeError as e:
 				print('AttributeError:', e)
-	date = datetime.now() + timedelta(days=-7)
-	cursor.execute("DELETE FROM homeworktable WHERE dayname=%s and daymonth=%s and dayYear=%s", 
-	(date.strftime('%d'), date.strftime('%m'), date.strftime('%Y'))
+	date = datetime.now(pytz.timezone('Asia/Yekaterinburg')) + timedelta(days=-7)
+	#countLinesAfter = cursor.execute('SELECT count(*) FROM homeworktable;')
+	cursor.execute("DELETE FROM homeworktable WHERE dayname=%s and daymonth=%s and dayYear=%s", (int(date.strftime(' %d').replace(' 0', '')), int(date.strftime(' %m').replace(' 0' '')), date.strftime('%Y'))
 	)
 	connection.commit()
+	if countLines != countLinesAfter:
+		return "New"
+	else:
+		return "Old"
 
 def selectHomework(day=1):
-	if day == 0:
-		date = datetime.now()
-	if day == 1:
-		date = datetime.now() + timedelta(days=1)
-	if day == -1:
-		date = datetime.now() + timedelta(days=-1)
-
-	if time.strftime('%m') == '06' or time.strftime('%m') == '07' or time.strftime('%m') == '08':
-		return 'Какая домаха, лето жеж'
-	if time.strftime('%w') == 6:
-		date = datetime.now() + timedelta(days=2)
-
 	connection = psycopg2.connect(os.getenv('DATABASE_URL'), sslmode='require')
 	cursor = connection.cursor()
-	cursor.execute(
-            'SELECT lesson, homework FROM homeworktable WHERE daynum=%s and daymonth=%s and dayYear=%s;',
-            (date.strftime('%d'), date.strftime('%m'), date.strftime('%Y'))
-    )
-	return '\n'.join(map(lambda x: '{}: {}'.format(x[0], x[1]), cursor.fetchall()))
+	if len(str(day)) == 0:
+		return 'Чтобы выбрать день нужно после команды указать дату в формате ```день.месяц.год```'
+	elif len(str(day)) > 2:
+		try: 
+			cursor.execute(
+				'SELECT lesson, homework FROM homeworktable WHERE daynum=%s and daymonth=%s and dayYear=%s;',
+				(
+					day.split('.')[0],
+					day.split('.')[1],
+					day.split('.')[2]
+				)
+			)
+			date = f"{day.split('.')[0]}.{day.split('.')[1]}.{day.split('.')[2]}"
+		except IndexError:
+			return 'Неподдерживаемый формат даты. Пример даты:\n```/s день.месяц.год```'
+		except psycopg2.Error as e:
+			return 'Возможно вы пытаетесь получить слишком раннюю домаху, т.к. бот хранит только последние 7 дней домашки '
+	else:
+		date = datetime.now(pytz.timezone('Asia/Yekaterinburg')) + timedelta(days=int(day))
+		for i in summerHolidays:
+			if date.strftime('%m') == i:
+				return 'Какая домаха, лето жеж'
+		for i in holidays:
+			if date.strftime('%d.%m.%Y') == i:
+				return 'Какая домаха, каникулы жеж'
+		if date.strftime('%w') == 0:
+			date = datetime.now(pytz.timezone('Asia/Yekaterinburg')) + timedelta(days=2) 
+		try:
+			cursor.execute(
+	            'SELECT lesson, homework FROM homeworktable WHERE daynum=%s and daymonth=%s and dayYear=%s;',
+	            (int(date.strftime(' %d').replace(' 0', '')), int(date.strftime(' %m').replace(' 0' '')), date.strftime('%Y'))
+			)
+		except TypeError:
+			cursor.execute(
+				'SELECT lesson, homework FROM homeworktable WHERE daynum=%s and daymonth=%s and dayYear=%s;',
+	            (date.strftime('%d'), date.strftime('%m'), date.strftime('%Y'))
+			)
+	d = date.strftime('%d.%m.%Y')
+	a = f'Домаха на ```{d}```: \n' + '\n'.join(map(lambda x: f'**{x[0]}**: {x[1]}', cursor.fetchall()))
+	return a
+
--- telegramBot.py
+++ telegramBot.py
@@ -1,15 +1,64 @@
 import telebot
 from telebot import types
-import os 
 from extractHomeworkFromHTML import extractHomework, selectHomework
+import os
 
-bot = telebot.TeleBot("1950280557:AAEGpmKCGIHZsoxzH5ndIJDTB5rMdGPoLN8", parse_mode=None)
+bot = telebot.TeleBot(os.getenv("TELEGRAM_API_TOKEN"), parse_mode='Markdown')
 
-@bot.message_handler(commands=['che'])
-def send_welcome(message):
-	bot.reply_to(message, selectHomework())
-@bot.inline_handler(func=lambda query: len(query.query) > 0)
-def query_text(query):
+listLessons = '2 сентября будет: \n8:00-8:40 -  Матан\n8:45-9:25 - ОБЖ\n9:35-10:15 - История\n10:25-11:05 - Русский\n'
+markup = types.ReplyKeyboardMarkup()
+
+@bot.message_handler(commands=['che', 'Che'])
+def send_che(message):
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    bot.reply_to(message, selectHomework())
+
+@bot.message_handler(commands=['yesterday'])
+def send_yesterday(message):
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    bot.reply_to(message, selectHomework(-1))
+
+@bot.message_handler(commands=['today'])
+def send_today(message):
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    bot.reply_to(message, selectHomework(0))
+
+@bot.message_handler(commands=['lessons'])
+def sendListOfLessons(message):
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    bot.reply_to(
+    	message,
+    	open('lessons.txt', 'r', encoding= 'utf-8'.read())
+    )
+
+@bot.message_handler(commands=['set'])
+def setLessons(message):
+	open('lessons.txt', 'w').write(message.text.split(' ')[1])
+	bot.reply_to(message, 'Расписание сохранено')
+	
+@bot.message_handler(commands=['help', 'start'])
+def send_help(message):
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    bot.reply_to(message, 'Даров :)\nТы попал к боту, который достанет тебе домашку из Сетевого Города и скинет тебе.\nчтобы воспользоваться моей основной функцией напиши/che')
+
+@bot.message_handler(commands=['select'])
+def s(message):
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    try:
+	    bot.reply_to(
+		message, 
+		selectHomework(message.text.split(' ')[1]),
+		parse_mode="Markdown"
+	    )
+    except IndexError:
+        bot.reply_to(
+            message,
+            'Чтобы воспользоваться этой командой, надо указать дату в формате ```день.месяц.год```\nP.S. Бот хранит домашку только за последние 7 дней',
+            parse_mode="Markdown"
+            )
+
+@bot.inline_handler(func=lambda query: len(query.query) >= 0)
+def query_text(message):
     try:
         che = types.InlineQueryResultArticle(
             id='1', title="Че",
@@ -19,20 +68,36 @@ def query_text(query):
             )
         )
         today = types.InlineQueryResultArticle(
-            id='2', title='сегодня',
+            id='3', title='сегодня',
             description='узнать домаху на сегодня',
             input_message_content=types.InputTextMessageContent(
                 message_text=selectHomework(0)
             )
         )
         yesterday = types.InlineQueryResultArticle(
-            id='3', title='вчера',
-            description='узнать вчеранюю домаху',
+            id='4', title='вчера',
+            description='узнать вчерашнюю домаху',
             input_message_content=types.InputTextMessageContent(
                 message_text=selectHomework(-1)
             )
         )
-        bot.answer_inline_query(query.id, [che, today, yesterday])
+        lessons = types.InlineQueryResultArticle(
+        	id='2', title='расписание',
+        	description='узнать расписание',
+        	input_message_content=types.InputTextMessageContent(message_text=listLessons)
+        	)
+        bot.answer_inline_query(message.id, [che, lessons, today, yesterday])
     except Exception as e:
-        print(e)
+    	bot.send_message(401311369, message + '\n\n' + e)
+    	bot.reply_to(message, 'Упс, что то пошло не так :(')
+    	print(e)
+    print(str(message.from_user.id) +' ' + str(message.from_user.username)+ ' '+ str(message.chat.id) + ' ' + str(message.text))
+    
+bot.send_message(401311369, 'все ок')
 bot.polling()
+
+def sendHomework(message=selectHomework()):
+    print('send homework at 14:30')
+    a = bot.send_message('-1001561236768', message)
+    bot.pin_chat_message('-1001561236768', a.id, disable_notification=True)
+    bot.pinChatMessage('-1001561236768', a.id, disable_notification=True)

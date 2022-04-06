ee6858922bc97ebc22748ab8b1eefbe925f8f049
mezhendosina <mensh.zheya@gmail.com>
2022-04-06 14:56:47 +0500

    add display of attendants

--- telegramBot.py
+++ telegramBot.py
@@ -12,6 +12,25 @@ connection = psycopg2.connect(os.getenv('DATABASE_URL'), sslmode='require')
 cursor = connection.cursor()  # connect to database
 
 
+def current_pidor() -> str:
+    cursor.execute("SELECT * FROM current_duty")
+    time_now = datetime.now()
+    current_duty = cursor.fetchall()[0]
+
+    if time_now.strftime("%d.%m.%Y") > current_duty[1] and time_now.strftime("%w") != 0:
+        if current_duty[0] > 14:
+            a = 0
+        else:
+            a = current_duty[0] + 1
+        cursor.execute(f"UPDATE current_duty SET id = {a}, date = '{time_now.strftime('%d.%m.%Y')}'")
+        connection.commit()
+        cursor.execute(f"SELECT people FROM duty WHERE id = {a}")
+    else:
+        cursor.execute(f"SELECT people FROM duty WHERE id = {current_duty[0]}")
+    b = cursor.fetchall()[0][0].split(',')
+    return f'<b>{b[0]}\n{b[1]}</b>'
+
+
 def telegram_bot():
     @bot.message_handler(commands=['help', 'start'])
     def send_help(message):
@@ -21,7 +40,11 @@ def telegram_bot():
         connection.commit()
         bot.reply_to(
             message,
-            'Это бот, который скидывает д\з \n<b>Список команд</b>\n/che - д\з на завтра\n/lessons - расписание\n/all_week - д\з на неделю'
+            'Это бот, который скидывает д\з \n'
+            '<b>Список команд</b>\n'
+            '/che - д\з на завтра\n'
+            '/lessons - расписание\n'
+            '/all_week - д\з на неделю'
         )
 
     @bot.message_handler(commands=['stats'])
@@ -42,13 +65,15 @@ def telegram_bot():
 
         bot.send_message(message.chat.id, select_homework())
 
-    # @bot.message_handler(commands=['all_week'])
-    # def send_all_week(message):
-    #     date = datetime.now(pytz.timezone('Asia/Yekaterinburg')).strftime('%Y.%m.%d %H:%M:%S')
-    #     cursor.execute(
-    #         f"INSERT INTO stats VALUES({message.from_user.id}, '{message.from_user.username}', '{message.text}', '{date}')")
-    #     connection.commit()
-    #     bot.send_message(message.chat.id, select_homework('all_week'))
+    @bot.message_handler(commands=['pidors_today'])
+    def send_pidor_day(message):
+        pidor_today = current_pidor()
+        date = datetime.now(pytz.timezone('Asia/Yekaterinburg')).strftime('%Y.%m.%d %H:%M:%S')
+        cursor.execute(
+            f"INSERT INTO stats VALUES({message.from_user.id}, '{message.from_user.username}', '{message.text}', '{date}')")
+        connection.commit()
+
+        bot.send_message(message.chat.id, f'<s>Пидоры дня</s> Дежурные сегодня (Beta):\n{pidor_today}')
 
     @bot.message_handler(commands=['lessons'])
     def send_list_of_lessons(message):
@@ -57,10 +82,7 @@ def telegram_bot():
             f"INSERT INTO stats VALUES({message.from_user.id}, '{message.from_user.username}', '{message.text}', '{date}')")
         connection.commit()
         text = open('files/lessons.txt', 'r', encoding='utf-8').read()
-        bot.reply_to(
-            message,
-            text
-        )
+        bot.reply_to(message, text)
 
     @bot.message_handler(commands=['некит'])
     def n(message):
@@ -70,37 +92,6 @@ def telegram_bot():
         connection.commit()
         voice = open('files/voice.ogg', 'rb')
         bot.send_voice(message.chat.id, voice)
-
-    @bot.inline_handler(func=lambda query: len(query.query) > 0)
-    def query_text(message):
-        try:
-            che = types.InlineQueryResultArticle(
-                id='1', title="Че",
-                description='узнать домаху на завтра',
-                input_message_content=types.InputTextMessageContent(
-                    message_text=select_homework(),
-                    parse_mode='Markdown'
-                )
-            )
-            lessons = types.InlineQueryResultArticle(
-                id='2', title='расписание',
-                description='узнать расписание',
-                input_message_content=types.InputTextMessageContent(
-                    message_text='Расписание\n' + open('files/lessons.txt', 'r', encoding='utf-8').read(),
-                    parse_mode='Markdown'
-                )
-            )
-            all_week = types.InlineQueryResultArticle(
-                id='2', title='вся неделя',
-                description='узнать д/з на всю неделю',
-                input_message_content=types.InputTextMessageContent(
-                    message_text=select_homework(day='all_week'),
-                    parse_mode='Markdown'
-                )
-            )
-            bot.answer_inline_query(message.id, [che, lessons, all_week])
-        except Exception as e:
-            print(e)
 
     bot.polling(non_stop=True)
 

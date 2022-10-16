4688d19931564e9f050df6b43166a75e4cac1776
mezhendosina <mensh.zheya@gmail.com>
2022-10-16 09:31:42 +0500

    add bio_ege

--- telegramBot.py
+++ telegramBot.py
@@ -140,24 +140,27 @@ def telegram_bot():
         russ_ege = datetime(2023, 5, 29, tzinfo=pytz.timezone('Asia/Yekaterinburg')) - time_now
         inf_ege = datetime(2023, 6, 19, tzinfo=pytz.timezone('Asia/Yekaterinburg')) - time_now
         phys_ege = datetime(2023, 6, 5, tzinfo=pytz.timezone('Asia/Yekaterinburg')) - time_now
+        bio_ege = datetime(2023, 6, 13, tzinfo=pytz.timezone('Asia/Yekaterinburg')) - time_now
         text = f"До ЕГЭ по математике <tg-spoiler>{math_ege.days} дней</tg-spoiler>\n" \
                f"До ЕГЭ по русскому <tg-spoiler>{russ_ege.days} дней</tg-spoiler>\n" \
                f"До ЕГЭ по информатике <tg-spoiler>{inf_ege.days} дней</tg-spoiler>\n" \
                f"До итогового сочинения <tg-spoiler>{soch.days} дней</tg-spoiler>\n" \
-               f"До <del>смерти Мишани</del> ЕГЭ по физике <tg-spoiler>{phys_ege.days} дней</tg-spoiler>"
+               f"До <del>смерти Мишани</del> ЕГЭ по физике <tg-spoiler>{phys_ege.days} дней</tg-spoiler>" \
+               f"До ЕГЭ по биологии <tg-spoiler>{bio_ege.days} дней</tg-spoiler>\n"
         bot.reply_to(message, text)
 
-    # special command for my friend
-    @bot.message_handler(commands=['некит'])
-    def n(message):
-        voice = open('files/voice.ogg', 'rb')
-        bot.send_voice(message.chat.id, voice)
 
-        report_activity(message)
+# special command for my friend
+@bot.message_handler(commands=['некит'])
+def n(message):
+    voice = open('files/voice.ogg', 'rb')
+    bot.send_voice(message.chat.id, voice)
 
-    # start bot
-    bot.polling(non_stop=True)
+    report_activity(message)
 
+
+# start bot
+bot.polling(non_stop=True)
 
 if __name__ == '__main__':
     telegram_bot()
